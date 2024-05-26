e1575381098b0cf7356fa0fb4bd3e8fe762ecad6
DoctorLimitless <safou348@gmail.com>
2024-05-26 17:43:34 +0100

    profile in user auth working

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,8 +2,15 @@
 import React, { FormEvent, useEffect, useRef, useState } from "react";
 import { Navbar } from "../Navbar";
 import { api } from "@/convex/_generated/api";
-import { useMutation, useQuery } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
 import Image from "next/image";
+
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
 
 export default function Profile() {
   const [title, setTitle] = useState("");
@@ -79,73 +86,89 @@ export default function Profile() {
   return (
     <div className="fixed inset-0">
       <Navbar />
-      <div className="h-full flex flex-col justify-center items-center">
-        <form
-          className="flex flex-col w-full p-5 gap-2 "
-          onSubmit={handleOnSubmit}
-        >
-          <input
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="bg-emerald-100 p-2"
-            placeholder="Title"
-          />
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="bg-emerald-100 p-2"
-            placeholder="Description"
-          />
-          <input
-            type="file"
-            accept="image/*"
-            ref={imageInput}
-            onChange={handleImageChange}
-          />
-          {selectedImage && (
-            <button
-              type="button"
-              onClick={handleRemoveImage}
-              className="bg-red-500 text-white p-2 rounded-lg mt-2"
-            >
-              الغاء الصورة
-            </button>
-          )}
-          <button
-            type="submit"
-            className="bg-emerald-800 text-white p-2 rounded-lg"
-            disabled={!selectedImage || isUploading}
+
+      <Authenticated>
+        <div className="h-full flex flex-col justify-center items-center">
+          <form
+            className="flex flex-col w-full p-5 gap-2 "
+            onSubmit={handleOnSubmit}
           >
-            تحميل
-          </button>
-          {isUploading ? (
-            <div className="absolute top-0 right-0 h-screen w-screen   bg-black opacity-70 flex justify-center items-center">
-              <p className=" text-white  text-2xl font-bold">...جاري التحميل</p>
-            </div>
-          ) : (
-            <></>
-          )}
-        </form>
-        {imagePreviewUrl && (
-          <div className="mt-4">
-            <Image
-              width={500}
-              height={500}
-              src={imagePreviewUrl}
-              alt="Image Preview"
-              className="max-w-xs"
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="bg-emerald-100 p-2"
+              placeholder="Title"
             />
-          </div>
-        )}
-        {isUploading && (
-          <div className="w-full bg-gray-200 rounded-full h-2.5 mt-2">
-            <div
-              className="bg-blue-600 h-2.5 rounded-full"
-              style={{ width: `${uploadProgress}%` }}
-            ></div>
-          </div>
-        )}
-      </div>
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="bg-emerald-100 p-2"
+              placeholder="Description"
+            />
+            <input
+              type="file"
+              accept="image/*"
+              ref={imageInput}
+              onChange={handleImageChange}
+            />
+            {selectedImage && (
+              <button
+                type="button"
+                onClick={handleRemoveImage}
+                className="bg-red-500 text-white p-2 rounded-lg mt-2"
+              >
+                الغاء الصورة
+              </button>
+            )}
+            <button
+              type="submit"
+              className="bg-emerald-800 text-white p-2 rounded-lg"
+              disabled={!selectedImage || isUploading}
+            >
+              تحميل
+            </button>
+            {isUploading ? (
+              <div className="absolute top-0 right-0 h-screen w-screen   bg-black opacity-70 flex justify-center items-center">
+                <p className=" text-white  text-2xl font-bold">
+                  ...جاري التحميل
+                </p>
+              </div>
+            ) : (
+              <></>
+            )}
+          </form>
+          {imagePreviewUrl && (
+            <div className="mt-4">
+              <Image
+                width={500}
+                height={500}
+                src={imagePreviewUrl}
+                alt="Image Preview"
+                className="max-w-xs"
+              />
+            </div>
+          )}
+          {isUploading && (
+            <div className="w-full bg-gray-200 rounded-full h-2.5 mt-2">
+              <div
+                className="bg-blue-600 h-2.5 rounded-full"
+                style={{ width: `${uploadProgress}%` }}
+              ></div>
+            </div>
+          )}
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <div className="flex items-center flex-col">
+          <h2 className="bg-green-900 p-2 rounded-lg text-white text-xl font-bold mb-4">
+            <SignUpButton />
+          </h2>
+
+          <h2 className="bg-green-900 p-2 rounded-lg text-white text-xl font-bold mb-4">
+            <SignInButton />
+          </h2>
+        </div>
+      </Unauthenticated>
     </div>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://massive-sawfish-8.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
