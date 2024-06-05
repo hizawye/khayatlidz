9adb85ca54dda318cee9293cd5adeb8a0b8b2e17
DoctorLimitless <safou348@gmail.com>
2024-06-05 18:35:15 +0100

    fixing void[] problems

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useRouter } from "next/navigation";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
@@ -7,12 +6,13 @@ import { Navbar } from "@/app/Navbar";
 import { v } from "convex/values";
 
 interface Params {
-  postId: any; // Assuming postId is of type string
+  postId: string; // Specify type as string
 }
 
-export default function PostDetailes({ params }: { params: Params }) {
+export default function PostDetails({ params }: { params: Params }) {
   const router = useRouter();
   const postId = params.postId;
+
   const post = useQuery(api.posts.getPost, { postId });
 
   if (post === undefined) {
@@ -26,27 +26,26 @@ export default function PostDetailes({ params }: { params: Params }) {
   return (
     <>
       <Navbar />
-      {post?.map((post, index) => {
+      {post?.map((post, index) => (
         <div key={index} className="container mx-auto p-4">
           <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
           <div className="w-full max-w-md mx-auto">
-            {post?.imageUrls?.map((url, index) => {
-              <Image
-                key={index}
-                src={url!}
-                alt={post.title}
-                width={500}
-                height={500}
-                className="object-cover rounded-lg shadow-md"
-              />;
-            })}
+            {post?.imageUrls?.length > 0 && // Check if imageUrls has elements
+              post.imageUrls.map((url, index) => (
+                <Image
+                  key={index}
+                  src={url || "path/to/placeholder.jpg"} // Placeholder for missing url
+                  alt={post.title}
+                  width={500}
+                  height={500}
+                  className="object-cover rounded-lg shadow-md"
+                />
+              ))}
           </div>
           <p className="mt-4 text-gray-700">{post.description}</p>
           {/* Add more details as needed */}
-        </div>;
-      })}
+        </div>
+      ))}
     </>
   );
 }
-
-//TODO adding a state of where user came from ,add button to get him back
